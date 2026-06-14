175b89cdfabf141013cd9d539181c1b424962e39
Suman Mandal <connect.sumanmandal@gmail.com>
2026-06-14 22:54:43 +0530

    Initial Commit: LAB 1 and LAB 2 Commit

---
+++ UNIT1/LAB-1/home.php
@@ -0,0 +1,7 @@
+<?php
+    echo "<title>Home Page | PHP Lab 1</title>";
+    echo "<center>";
+    echo "<h1>Home Page</h1>";
+    echo "<p>Welcome to the home page!</p>";
+    echo "</center>";
+?>
---
+++ UNIT1/LAB-1/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+    <center>
+        <h1>Login Page</h1>
+        <form method="POST" action='login.php'>
+            Enter Username: <input type='text' id='username' name='username' required><br><br>
+            Enter Password: <input type='password' id='password' name='password' required><br><br>
+            <input type='submit' value='Login'>
+        </form>
+    </center>
+</body>
+</html>
+
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if ($username === "suman" && $password === "1234") 
+        } else {
+            echo "<center><h2 style='color: red;'>Login failed!</h2></center>";
+        }
+    }
+?>

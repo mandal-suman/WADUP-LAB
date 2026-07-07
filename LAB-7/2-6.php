8f3a2f0d678a574bcae08b4efe2bbe88e092d2e5
Suman Mandal <connect.sumanmandal@gmail.com>
2026-07-07 08:27:53 +0530

    LAB-7 | 2.6 | Commit

--- LAB-7/2-6.php
+++ LAB-7/2-6.php
@@ -1,7 +1,25 @@
-<!-- 2.6 Write a PHP code to use mysql date and time functions as given bellow:
+<!-- 2.6 Write a PHP code to use date and time functions as given bellow:
         1) DAYOFWEEK()
         2) WEEKDAY()
         3) DAYOFMONTH()
         4) DAYOFYEAR()
         5) DAYNAME() -->
 
+<?php
+$date = "2024-01-15";
+
+$dayOfWeek = date('w', strtotime($date));
+echo "<b>DAYOFWEEK:</b> " . $dayOfWeek . "\n";
+
+$weekday = date('w', strtotime($date));
+echo "<br><b>WEEKDAY:</b> " . $weekday . "\n";
+
+$dayOfMonth = date('d', strtotime($date));
+echo "<br><b>DAYOFMONTH:</b> " . $dayOfMonth . "\n";
+
+$dayOfYear = date('z', strtotime($date)) + 1;
+echo "<br><b>DAYOFYEAR:</b> " . $dayOfYear . "\n";
+
+$dayName = date('l', strtotime($date));
+echo "<br><b>DAYNAME:</b> " . $dayName . "\n";
+?>  

d736211ff2554ea450fa804efc1da135162f38c5
Suman Mandal <connect.sumanmandal@gmail.com>
2026-08-18 08:03:46 +0530

    LAB 14 | Definition 2 | Commit

---
+++ LAB-14/2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>jQuery Background Color</title>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+</head>
+
+<body>
+
+    <h1>jQuery Example</h1>
+
+    <h2>This is a heading</h2>
+
+    <p>This is the first paragraph.</p>
+    <p>This is the second paragraph.</p>
+
+    <button>Click Me</button>
+
+    <script>
+        $(document).ready(function() {
+            $("h2, p, button").css("background-color", "red");
+        });
+    </script>
+
+</body>
+
+</html>
